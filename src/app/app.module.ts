cd588c50de524013e7118b67d09238bd4cbe6c57
Kurapati <kurapatl@ES.AD.ADP.COM>
2019-08-07 08:40:03 +0530

    Side bar changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    SidenavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
