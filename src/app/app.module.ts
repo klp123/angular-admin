16c673e4e7b71636c5279935a98d137c9246ac28
Kurapati <kurapatl@ES.AD.ADP.COM>
2019-08-10 18:01:55 +0530

    routing changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,16 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SearchComponent } from './search/search.component';
 import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  { path: '',redirectTo: '/home', pathMatch: 'full'},
+  { path: 'profile', component: ProfileComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
@@ -15,13 +25,18 @@ import { FooterComponent } from './footer/footer.component';
     NavbarComponent,
     SearchComponent,
     SidenavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ProfileComponent,
+    MessagesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
